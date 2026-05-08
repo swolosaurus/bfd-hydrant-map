878f81ad184263b92ebdab87ff61973a30ec8567
swolosaurus <alexander.cho.617@gmail.com>
2026-05-08 16:21:08 +0200

    v60: restore Vector mapId (referrer fix in Cloud Console resolved blank map)

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'bfd-hydrants-v59';
+const CACHE = 'bfd-hydrants-v60';
 const SHELL = ['./','./index.html','./manifest.json','./privacy.html'];
 
 self.addEventListener('install', e => {
